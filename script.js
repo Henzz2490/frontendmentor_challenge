1d89827096499301e74d7d05999e8516b299af74
Henzz2490 <setiawanh@proton.me>
2026-03-18 11:40:11 +0200

    Revision on social media share btn interaction

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('.btn');
-const shareIcons = document.querySelector('.soc-med-icon')
+const shareIcons = document.querySelector('.soc-med-icon');
 
-button.addEventListener("click", function(){
-    shareIcons.classList.toggle('is-open');
-})
+button.addEventListener('click', function () {
+  const isOpen = shareIcons.classList.toggle('is-open');
+  button.setAttribute('aria-expanded', isOpen);
+});
